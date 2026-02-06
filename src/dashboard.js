ceae5d4b732d8c402d7f2025c9e981f3af2bbdbf
aPuppyGod <tfuenot12321@gmail.com>
2026-02-06 22:04:11 +0000

    Fix ReferenceError: session is not defined by adding require('express-session') at top

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,7 @@
 
 
 const express = require("express");
+const session = require("express-session");
 function htmlTemplate(content, opts = {}) {
   // opts: { user, isAdmin, active }
   const user = opts.user;
